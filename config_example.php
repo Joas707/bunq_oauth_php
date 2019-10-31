ec32b69718aaf7825af85fb22de64e489fd4495b
Joas <joas707@live.nl>
2019-10-31 15:26:10 +0100

    add config_example file

---
+++ config_example.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+    'filepath_for_bunqConf.conf' => 'YOUR_SAFE_DIRECTORY',
+    'redirect_url_for_api_token' => 'https://YOUR_REDIRECT_URL.com/get_api_token.php',
+    'client_id' => 'YOUR_CLIENT_ID',
+    'client_secret' => 'YOUR_CLIENT_SECRET'
+);
