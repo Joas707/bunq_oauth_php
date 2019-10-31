c950e717c249d1180d7fe0b158a921ee14c5a5a9
Joas <joas707@live.nl>
2019-10-31 15:27:08 +0100

    made comments nice

--- generate_conf_file.php
+++ generate_conf_file.php
@@ -2,17 +2,16 @@
 session_start();
 $configs = include('config.php');
 use bunq\Context\ApiContext;
-//use bunq\Context\BunqContext;
 use bunq\Util\BunqEnumApiEnvironmentType;
 require_once __DIR__ . '/vendor/autoload.php';
 
-//check if the access token is saved by get_api_token.php and if it isn't then show an error.
+// Check if the access token is saved by get_api_token.php and if it isn't then show an error.
 if(!isset($_SESSION['access_token'])){
   echo "The access token isn't set.";
   exit();
 }
 
-//if the access token is saved ask for a device description
+// If the access token is saved ask for a device description. I have no clue what bunq does with it or were to find it myself.
 if(!isset($_POST['description'])){
 echo "<html>
 <body>
@@ -26,7 +25,7 @@ Device description: <input type=\"text\" name=\"description\"><br>
 </html>
 ";
 }else{
-//make the configuration file according to the readme of bunq
+// Make the configuration file according to the readme of bunq
 $environmentType = BunqEnumApiEnvironmentType::PRODUCTION(); // Can also be BunqEnumApiEnvironmentType::PRODUCTION();
 $apiKey = $_SESSION['access_token']; // Replace with your API key
 $deviceDescription = $_POST['description']; // Replace with your device description
@@ -39,12 +38,10 @@ $apiContext = ApiContext::create(
     $permittedIps
 );
 
-//for safety reasons; this program will never try to make a conf file with the same access token twice
+// For safety reasons; this program will never try to make a conf file with the same access token twice
 unset($_SESSION['access_token']);
-
-//BunqContext::loadApiContext($apiContext);
 
 $fileName = ''. $configs['filepath_for_bunqConf.conf'] .'/bunqConf.conf'; // Replace with your own secure location to store the API context details
 $apiContext->save($fileName);
-echo "Your configuration file is saved. For security reasons it is not shown where.";
+echo "It seems that your configuration file is saved. For security reasons the directory is not shown.";
 }
--- get_api_token.php
+++ get_api_token.php
@@ -1,21 +1,13 @@
 <?php
 session_start();
 $configs = include('config.php');
+// Get the code from bunq
 $bunqcode = $_GET['code'];
 
 $ch = curl_init();
-
+// Ask bunq for the api token
 curl_setopt($ch, CURLOPT_URL,"https://api.oauth.bunq.com/v1/token?grant_type=authorization_code&code=". $bunqcode ."&redirect_uri=". $configs['redirect_url_for_api_token'] ."&client_id=". $configs['client_id'] ."&client_secret=". $configs['client_secret'] ."");
 curl_setopt($ch, CURLOPT_POST, 1);
-//curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, 'GET' );
-//curl_setopt($ch, CURLOPT_POSTFIELDS,
-//            "");
-
-// In real life you should use something like:
-// curl_setopt($ch, CURLOPT_POSTFIELDS,
-//          http_build_query(array('postvar1' => 'value1')));
-
-// Receive server response ...
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $server_output = curl_exec($ch);
@@ -23,6 +15,8 @@ $server_output = curl_exec($ch);
 curl_close ($ch);
 $server_output_readable = json_decode($server_output);
 $apikey = $server_output_readable->access_token;
+// If the api key came back then save it in a session variable and redirect to the other file.
+// Otherwise display an error.
 if($apikey != NULL){
   $_SESSION['access_token'] = $server_output_readable->access_token;
   header("location: /generate_conf_file.php");
